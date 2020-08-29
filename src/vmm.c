311e4c8a025d4b79bf66e8c8aa44006919c99a82
Mate Kukri <kukri.mate@gmail.com>
2020-08-29 12:38:54 +0100

    Prevent gcc from messing up the stack

--- src/vmm.c
+++ src/vmm.c
@@ -10,16 +10,60 @@
 #include "uart.h"
 #include "x86.h"
 
-/* The VMCB attrib bits are 55:52 and 47:40 from here */
-
 static
 uint64_t
 gdt[] = {
 	0,
-	0,
-	0x00209A0000000000,	/* __BOOT_CS */
-	0x0000920000000000,	/* __BOOT_DS */
+	0x00209A0000000000,	/* CS */
+	0x0000920000000000,	/* DS */
 };
+
+static
+void
+vmm_load_gdt(void)
+{
+	struct {
+		uint16_t limit;
+		uint64_t addr;
+	} __attribute__((packed)) gdtr;
+
+	gdtr.limit = sizeof(gdt);
+	gdtr.addr = (uint64_t) gdt;
+
+	asm volatile ("lgdt %0\n"
+			"pushq $0x08\n"
+			"pushq $reload_cs\n"
+			"retfq; reload_cs:\n"
+			"movl $0x10, %%eax\n"
+			"movl %%eax, %%ds\n"
+			"movl %%eax, %%es\n"
+			"movl %%eax, %%ss\n"
+			"movl %%eax, %%fs\n"
+			"movl %%eax, %%gs"
+			 :: "m" (gdtr) : "rax");
+}
+
+void vmm_pml4();
+void vmm_pdp();
+
+static
+void
+vmm_switchpg()
+{
+	uint64_t *pml4, *pdp, cur_phys;
+	size_t i;
+
+	pml4 = (uint64_t *) vmm_pml4;
+	pdp = (uint64_t *) vmm_pdp;
+	cur_phys = 0;
+
+	for (i = 0; i < 512; ++i) {
+		pdp[i] = cur_phys | 0x83;
+		cur_phys += 0x40000000;
+	}
+	pml4[0] = (uint64_t) vmm_pdp | 3;
+	asm volatile ("movq %0, %%cr3" :: "r" (vmm_pml4));
+}
 
 /*
  * VMCB
@@ -48,29 +92,6 @@ void ge_boot_params();
 void
 vmm_run_guest();
 
-void vmm_pml4();
-void vmm_pdp();
-
-
-static
-void
-vmm_switchpg()
-{
-	uint64_t *pml4, *pdp, cur_phys;
-	size_t i;
-
-	pml4 = (uint64_t *) vmm_pml4;
-	pdp = (uint64_t *) vmm_pdp;
-	cur_phys = 0;
-
-	for (i = 0; i < 512; ++i) {
-		pdp[i] = cur_phys | 0x83;
-		cur_phys += 0x40000000;
-	}
-	pml4[0] = (uint64_t) vmm_pdp | 3;
-	asm volatile ("movq %0, %%cr3" :: "r" (vmm_pml4));
-}
-
 void
 vmm_startup(void *linux_entry, void *boot_params)
 {
@@ -92,24 +113,19 @@ vmm_startup(void *linux_entry, void *boot_params)
 	vmcb.vmrun = 1;
 	vmcb.cpuid = 1;
 
-	vmcb.gdtr_limit = sizeof(gdt);
-	vmcb.gdtr_base = (uint64_t) gdt;
-
+	/* FIXME: the current segmentation setup has no GDT backing it,
+		only hidden segmnet registers are set up, but Linux loads its
+		own so we don't care for now */
 	vmcb.es_selector = 0x18;
 	vmcb.es_attrib = 0x0092;
-
 	vmcb.cs_selector = 0x10;
 	vmcb.cs_attrib = 0x029a;
-
 	vmcb.ss_selector = 0x18;
 	vmcb.ss_attrib = 0x0092;
-
 	vmcb.ds_selector = 0x18;
 	vmcb.ds_attrib = 0x0092;
-
 	vmcb.fs_selector = 0x18;
 	vmcb.fs_attrib = 0x0092;
-
 	vmcb.gs_selector = 0x18;
 	vmcb.gs_attrib = 0x0092;
 
@@ -128,12 +144,12 @@ vmm_startup(void *linux_entry, void *boot_params)
 	uart_print("VMCB at: %p\n", &vmcb);
 	uart_print("Starting guest...\n");
 
+	vmm_load_gdt();
 	vmm_switchpg();
 	vmm_run_guest();
 }
 
 struct gpr_save {
-	u64 rax;
 	u64 rbx;
 	u64 rcx;
 	u64 rdx;
@@ -164,8 +180,6 @@ vmexit_handler(struct gpr_save *gprs)
 			&cpuid[1],
 			&cpuid[2],
 			&cpuid[3]);
-		uart_print("EAX=%x, EBX=%x, ECX=%x, EDX=%x\n",
-			cpuid[0], cpuid[1], cpuid[2], cpuid[3]);
 		if (!vmcb.rax) {
 			vmcb.rax = cpuid[0];
 			gprs->rbx = 0x746f6f42;
